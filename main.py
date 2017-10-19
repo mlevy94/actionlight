fe3c779178208262779cd951ffba5a528df5fd72
Michael Levy <mlevy94@vt.edu>
2017-10-18 21:25:05 -0400

    Moved screen rectangle calculation outside of loop

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from phue import Bridge
 import time
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 4
+K = 8
 regions = 2
 lights = ["Hue TV 1", "Hue TV 2"]
 
@@ -25,7 +25,6 @@ def regionprocess(light, imgQ, senderQ):
         weights = list(map(len, points))
         dominant = centers[weights.index(max(weights))]
         try:
-            # xy = converter.hex_to_xy(dominant)
             xy = converter.rgb_to_xy(*dominant)
         except ZeroDivisionError:
             continue
@@ -59,13 +58,13 @@ def main(addr):
     senderP = Process(target=senderprocess, args=[bridge, senderQ])
     senderP.start()
     processpool, queuepool = list(zip(*map(lambda x: makeprocess(*x), zip(lights, [senderQ] * regions))))
-
+    display = getDisplayRects()[1]
 
     # start = time.time()
     # for _ in range(15):
     while True:
         start = time.time()
-        imDisplay = getRectAsImage(getDisplayRects()[1])
+        imDisplay = getRectAsImage(display)
         list(map(lambda x: x.put(imDisplay), queuepool))
         sleeptime = 0.2 - (time.time() - start)
         if sleeptime > 0:
